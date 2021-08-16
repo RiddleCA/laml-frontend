45e199e6366f3d2188ec0ff9dc48244ebe9a4c85
Callum <callumarron@hotmail.com>
2021-08-16 16:30:25 +1200

    testing git cli changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="text-red-500">
       <h1>Hello world</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia aut ea, magni illum at fugiat ratione, quisquam officia quasi excepturi similique unde aliquid sed voluptate corporis odit. Commodi, labore officia.</p>
     </div>
   );
 }
