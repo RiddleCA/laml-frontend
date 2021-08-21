eb1799c45465f5ffc0b467af19be64ddc0733c0c
Callum <callumarron@hotmail.com>
2021-08-21 22:23:55 +1200

    changed nav styling

--- src/Components/Submit.js
+++ src/Components/Submit.js
@@ -5,6 +5,7 @@ const Submit = () => {
   return (
     <div className="bg-gray-300 bg-opacity-40 border-white border border-opacity-40 w-5/6 md:w-2/5 h-5/6 m-auto rounded-lg mt-16 md:mt-28 card">
       <div className="m-auto w-full p-8">
+      <img className="w-20 m-auto pb-6" src="../assets/slug-icon.png" alt="" />
         <div className="flex justify-around">
            <input className="rounded-full my-8 p-2 w-1/4 text-3xl text-purple-300 bg-purple-700 hover:bg-purple-500 hover:ring-purple-200 hover:ring" type="button" value="-" />
            <p className="text-purple-300 text-6xl ">{count}</p>
