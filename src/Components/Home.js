26981ab05d4e8b2faaf8dd79818c458b4773317a
Callum <callumarron@hotmail.com>
2021-08-21 21:48:53 +1200

    base submit button styling completed

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
         <input className="m-auto rounded-full my-2 p-3 w-full bg-gray-300 bg-opacity-40 text-purple-900 focus:ring-purple-200 focus:ring outline-none" type="text" name="eventReg" id="eventReg" /><br />
         <label className="m-auto text-purple-200 text-3xl" htmlFor="user">Username</label><br />
         <input className="m-auto rounded-full my-2 p-3 w-full bg-gray-300 bg-opacity-40 text-purple-900 focus:ring-purple-200 focus:ring outline-none" type="text" name="user" id="user" /><br />
-        <input className="m-auto rounded-full my-8 p-3 text-3xl text-purple-300 w-full  bg-purple-700 hover:bg-purple-500 hover:ring-purple-200 hover:ring " type="button" value="Join" />
+        <input className="m-auto rounded-full my-8 p-3 text-3xl text-purple-300 w-full  bg-purple-700 hover:bg-purple-500 hover:ring-purple-200 hover:ring" type="button" value="Join" />
       </div>     
     </div>
   );
--- src/Components/Submit.js
+++ src/Components/Submit.js
@@ -1,9 +1,17 @@
 import React from "react";
 
 const Submit = () => {
+  let count = 0;
   return (
-    <div>
-      <h2>Submit</h2>
+    <div className="bg-gray-300 bg-opacity-40 border-white border border-opacity-40 w-5/6 md:w-2/5 h-5/6 m-auto rounded-lg mt-16 md:mt-28 card">
+      <div className="m-auto w-full p-8">
+        <div className="flex justify-around">
+           <input className="rounded-full my-8 p-2 w-1/4 text-3xl text-purple-300 bg-purple-700 hover:bg-purple-500 hover:ring-purple-200 hover:ring" type="button" value="-" />
+           <p className="text-purple-300 text-6xl ">{count}</p>
+           <input className="rounded-full my-8 p-2 w-1/4 text-3xl text-purple-300 bg-purple-700 hover:bg-purple-500 hover:ring-purple-200 hover:ring" type="button" value="+" />
+        </div>
+        <input className="m-auto rounded-full my-8 p-2 text-3xl text-purple-300 w-full  bg-purple-700 hover:bg-purple-500 hover:ring-purple-200 hover:ring" type="button" value="Submit" />
+      </div>
     </div>
   );
 };
