69603237925ca17eafe8eef9bf85d836a80863cd
Callum <callumarron@hotmail.com>
2021-08-21 16:38:46 +1200

    tweaking home page ui, added slug icon

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,6 +5,7 @@ const Home = () => {
   return (
     <div className="bg-gray-300 w-5/6 md:w-2/5 h-5/6 m-auto rounded-lg mt-28">
       <div className="m-auto w-full p-8">
+        <img className="w-20 m-auto" src="./assets/slug-icon.png" alt="" />
         <label className="m-auto text-purple-700 text-3xl" htmlFor="eventReg">Event</label><br />
         <input className="m-auto rounded-full my-2 p-3 w-full text-purple-900" type="text" name="eventReg" id="eventReg" /><br />
         <label className="m-auto text-purple-700 text-3xl" htmlFor="user">Username</label><br />
