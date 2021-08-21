c67acf5885e073d9c6a54b0139dba6aacc2bddd0
Callum <callumarron@hotmail.com>
2021-08-21 14:30:34 +1200

    set up netlify redirects

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 const Home = () => {
   return (
     <div>
       <h2>Home</h2>
+      <Link className="text-red-500" to="/event"> Event</Link>
     </div>
   );
 };
