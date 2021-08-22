ae755849e9ade32146fa459c0cb54281f27f2e8c
Callum <callumarron@hotmail.com>
2021-08-22 17:16:16 +1200

    changed mobile responsoveness

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -4,9 +4,9 @@ import {Link} from "react-router-dom";
 const Nav = () => {
     return (
         <nav className="flex h-16 bg-purple-700 bg-opacity-40 card justify-around">
-            <Link className="my-auto px-4 py-2 text-purple-200 sm:hover:text-purple-700 sm:hover:bg-purple-200  rounded-full  font-extrabold" to="/">Home </Link>
-            <Link className="my-auto px-4 py-2 text-purple-200 sm:hover:text-purple-700 sm:hover:bg-purple-200  rounded-full font-extrabold" to="/event">Event </Link>
-            <Link className="my-auto px-4 py-2 text-purple-700 bg-purple-200 sm:bg-transparent sm:text-purple-200 sm:hover:text-purple-700 sm:hover:bg-purple-200  rounded-full font-extrabold" to="/submit">Submit </Link>
+            <Link className="my-auto px-4 py-2 text-purple-200 hover:text-purple-700 hover:bg-purple-200  rounded-full font-extrabold" to="/">Home </Link>
+            <Link className="my-auto px-4 py-2 text-purple-200 hover:text-purple-700 hover:bg-purple-200  rounded-full font-extrabold" to="/event">Event </Link>
+            <Link className="my-auto px-4 py-2 text-purple-200 hover:text-purple-700 hover:bg-purple-200  rounded-full font-extrabold" to="/submit">Submit </Link>
         </nav>
     )
 }
