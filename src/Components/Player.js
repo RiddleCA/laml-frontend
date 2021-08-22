d42347da735980e6a5aef75843dc06bfa6a345f0
Callum <callumarron@hotmail.com>
2021-08-23 09:53:18 +1200

    changing event details display

--- src/Components/Player.js
+++ src/Components/Player.js
@@ -4,7 +4,7 @@ const Player = ({player}) => {
     return (
         <div >
             <div className="bg-purple-300 bg-opacity-40 border-white border border-opacity-40 rounded-md my-2 card">
-                <p className="text-purple-600">{`${player.item_count} ${player.username}`}</p>
+                <p className="text-purple-600">{`${player.item_count} ${player.username}`}</p> <img src="../assets/slug-icon.png" className="w-20 m-auto" alt="" />
             </div>
             
         </div>
