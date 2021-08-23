698b862a4557f0759363acd106f109ab96f87a8d
Callum <callumarron@hotmail.com>
2021-08-23 13:05:16 +1200

    tweaking event ui

--- src/Components/Player.js
+++ src/Components/Player.js
@@ -4,7 +4,7 @@ const Player = ({player}) => {
     return (
         
          <div className="bg-purple-300 bg-opacity-40 border-white border border-opacity-40 rounded-md my-2 card w-5/6 first:w-full first:bg-yellow-500">
-            <p className="text-purple-600">{`${player.item_count} ${player.username}`}</p> <img src="../assets/slug-icon.png" className=" m-auto" alt="" />
+            <p className="text-purple-600">{`${player.item_count} ${player.username}`}</p> <img src="../assets/slug-icon.png" className="w-20 m-auto" alt="" />
         </div>
     )
 }
