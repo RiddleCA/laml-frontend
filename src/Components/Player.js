7fa26a47ec98cf9dd9caf8347d48e46d30353912
Callum <callumarron@hotmail.com>
2021-08-23 12:47:01 +1200

    tweaking event ui

--- src/Components/Player.js
+++ src/Components/Player.js
@@ -2,11 +2,9 @@ import React from 'react'
 
 const Player = ({player}) => {
     return (
-        <div >
-            <div className="bg-purple-300 bg-opacity-40 border-white border border-opacity-40 rounded-md my-2 card first:w-full w-5/6">
-                <p className="text-purple-600">{`${player.item_count} ${player.username}`}</p> <img src="../assets/slug-icon.png" className="w-20 m-auto" alt="" />
-            </div>
-            
+        
+         <div className="bg-purple-300 bg-opacity-40 border-white border border-opacity-40 rounded-md my-2 card first:w-full w-5/6">
+            <p className="text-purple-600">{`${player.item_count} ${player.username}`}</p> <img src="../assets/slug-icon.png" className="w-20 m-auto" alt="" />
         </div>
     )
 }
