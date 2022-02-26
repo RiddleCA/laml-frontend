5639c990045190ae9446a65c9ca6ef4a2b2da5cf
Callum <callumarron@hotmail.com>
2022-02-26 14:51:14 +1300

    changed color for visual aid

--- src/Components/Event.js
+++ src/Components/Event.js
@@ -14,9 +14,9 @@ const Event = ({players, eventInfo}) => {
   ):(
     <div className="bg-gray-300 bg-opacity-40 border-white border border-opacity-40 w-5/6 md:w-2/5 h-5/6 m-auto rounded-lg mt-16 md:mt-28 card">
       <div className="m-auto w-full p-8">
-        <h2 className="text-purple-200 text-4xl">{eventInfo.title}</h2>
-        <h3 className="text-purple-200">{eventInfo.description}</h3>
-        <p className="text-right text-purple-200">Target {eventInfo.item_max}</p>
+        <h2 className="text-black text-4xl">{eventInfo.title}</h2>
+        <h3 className="text-black">{eventInfo.description}</h3>
+        <p className="text-right text-yellow-300">Target {eventInfo.item_max}</p>
         <div className="">
           {players.map((player) => <Player player={player}/>)}
         </div>
--- src/Components/Submit.js
+++ src/Components/Submit.js
@@ -30,18 +30,18 @@ const Submit = ({playerCount, submitScore, event, username}) => {
   return playerCount === 0 ? (
     <div className="bg-gray-300 bg-opacity-40 border-white border border-opacity-40 w-5/6 md:w-2/5 h-5/6 m-auto rounded-lg mt-16 md:mt-28 card">
       <div className="m-auto w-full p-8">
-        <p className="text-purple-300">To access this page please sign into an Event</p>
+        <p className="text-yellow-300">To access this page please sign into an Event</p>
         <Link className="my-auto px-4 py-2 text-purple-200 sm:hover:text-purple-700 sm:hover:bg-purple-200  rounded-full  font-extrabold" to="/">Home </Link>
       </div>
     </div>
   ): (
     <div className="bg-gray-300 bg-opacity-40 border-white border border-opacity-40 w-5/6 md:w-2/5 h-5/6 m-auto rounded-lg mt-16 md:mt-28 card">
       <div className="m-auto w-full p-8">
-      <h2 className="text-purple-200">{username}</h2> 
+      <h2 className="text-yellow-300">{username}</h2> 
       <img className="w-20 m-auto pb-6" src="../assets/slug-icon.png" alt="" />
         <div className="flex justify-around">
            <input className="rounded-full my-8 p-2 w-1/4 text-3xl text-purple-300 bg-purple-700 hover:bg-purple-500 hover:ring-purple-200 hover:ring" type="button" value="-" onClick={decrement} />
-           <p className="text-purple-300 text-6xl ">{count}</p>
+           <p className="text-yellow-300 text-6xl ">{count}</p>
            <input className="rounded-full my-8 p-2 w-1/4 text-3xl text-purple-300 bg-purple-700 hover:bg-purple-500 hover:ring-purple-200 hover:ring" type="button" value="+" onClick={increment}/>
         </div>
         <input className="m-auto rounded-full my-8 p-2 text-3xl text-purple-300 w-full  bg-purple-700 hover:bg-purple-500 hover:ring-purple-200 hover:ring" type="button" value="Submit" onClick={onSubmit} />
